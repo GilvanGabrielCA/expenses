c6ce0f60608257cb32448e7a5d4e581e781812c3
Gilvan Gabriel <gilvangabriel@gmail.com>
2025-02-26 08:42:28 -0400

    feat(expenses): implementar função para deletar despesas

--- lib/main.dart
+++ lib/main.dart
@@ -76,6 +76,12 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.of(context).pop();
   }
 
+  _removeTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
+  }
+
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -105,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Chart(_recentTrnasactions),
-            TransactionList(_transactions),
+            TransactionList(_transactions, _removeTransaction),
           ],
         ),
       ),
